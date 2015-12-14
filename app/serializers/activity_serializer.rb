d7c9487b072f28da01c37e261db02d836c3afe9f
github username <hhl262@nyu.edu>
2015-12-14 15:10:14 -0500

    updated activity serializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,3 +1,4 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :action, :trackable_id, :trackable_type, :trackable_name, :trackable_source
+  attributes :id, :action, :trackable_id, :trackable_type, :trackable_name, :trackable_source, :user_id
+
 end
